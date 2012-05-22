f59a1075263b22c8322ea26784dd8b9b51923339
Thierry Zires <zshuaibin@gmail.com>
2012-05-22 20:36:55 +0800

    Add sinon js to test xmlhttprequest

--- src/dictionary.coffee
+++ src/dictionary.coffee
@@ -4,6 +4,7 @@ class Module
   @include: (obj) ->
     for key, value of obj when key not in moduleKeywords
       # Assign properties to the prototype
+      # @:: means this.prototype
       @::[key] = value
 
     obj.included?.apply(@)
@@ -11,18 +12,29 @@ class Module
 
 # YouDaoModule
 youDaoModule = 
-  translate: (word) ->
+  translate: (word, onSuccess, onFailure) ->
     # TODO: may be need some code for ie6
     xhr = new window.XMLHttpRequest()
     url = "http://fanyi.youdao.com/openapi.do?keyfrom=#{@options.keyfrom}&key=#{@options.key}&type=data&doctype=json&version=1.1&q=#{word}"
-    xhr.open('GET', url, true)
+    xhr.open('GET', url, onSuccess?)
+    if onSuccess?
+      failureCallback = onFailure ? ->
+      xhr.onreadystatechange = ->
+        onSuccess(xhr.responseText) if xhr.readyState == 4 and xhr.status == 200
+        failureCallback(xhr.responseText) if xhr.readyState == 4 and xhr.status != 200
     xhr.send null
-    xhr
+    unless onSuccess?
+      if xhr.status == 200
+        try
+          return eval("(#{xhr.responseText})").translation.join()
+        catch error
+          return null
+      else
+        return xhr.statusText
 
 class Dictionary extends Module
-  constructor: (@name, @options) ->
-    if @name == 'youdao'
-      Dictionary.include(youDaoModule)
+  constructor: (@name, @options = {}) ->
+    Dictionary.include(youDaoModule) if @name == 'youdao'
 
 # Globals
 exports = this
--- test/spec/dictionarySpec.js
+++ test/spec/dictionarySpec.js
@@ -1,11 +1,46 @@
 describe('Dictionary', function() {
 
   describe('when use youdao module', function() {
+    // This is a fake word should return from server
+    var yesterday = '昨天';
+    var dict;
 
-    it("should be able to translate word form english to chinese", function() {
-      var expectTranslation = {"phonetic":"'jestədi,-dei","explains":["n. 昨天；往昔","adv. 昨天"]};
-      var dict = new Dictionary('youdao', 'lala');
-      expect( dict.translate('yesterday') ).toBe(expectTranslation);
+    beforeEach(function () {
+      // This is a fake youdao server
+      this.server = sinon.fakeServer.create();
+
+      this.server.respondWith("GET", 
+        "http://fanyi.youdao.com/openapi.do?keyfrom=fackkeyfrom&key=fackkey&type=data&doctype=json&version=1.1&q=yesterday",
+        [200, { "Content-Type": "application/json" },'{"translation":["昨天"]}']);
+
+      dict = new Dictionary('youdao', {keyfrom: 'fackkeyfrom',key: 'fackkey'});
+
+    });
+
+    afterEach(function() {
+      this.server.restore();
+    });
+
+    it("should have right url", function() {
+      dict.translate('yesterday');
+      expect( this.server.requests.length ).toBe(1);
+      expect( this.server.requests[0].url ).toBe("http://fanyi.youdao.com/openapi.do?keyfrom=fackkeyfrom&key=fackkey&type=data&doctype=json&version=1.1&q=yesterday");
+    });
+
+    it("can be able to translate word form english to chinese simply (Synchronous)", function() {
+      expect( dict.translate('yesterday') ).toBe(yesterday);
+    });
+
+    it("can be able to translate word form english to chinese using success callback", function() {
+      var successCallback = sinon.spy();
+      dict.translate('yesterday', successCallback);
+      this.server.respond();
+      var text = this.server.requests[0].responseText
+      expect( successCallback.calledWith(text) ).toBeTruthy();
+    });
+
+    it("has a alias function t which like translate", function() {
+      expect( dict.translate('yesterday') ).toBe( dict.t('yesterday') );
     });
 
   });
